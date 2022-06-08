d9fd17506ee083c0aa955aa928df0c6c4e9c5ca3
Dhevano Gading <dhevanogading@gmail.com>
2022-06-08 09:38:33 +0700

    update commit

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,8 @@
 @section('container')
     <h1 class="mb-5">{{ $post->tittle }}</h1>
 
+    <p>By. Dhevano in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
     {!! $post->body !!}
 
     <a href="/posts">Back to Blog</a>
